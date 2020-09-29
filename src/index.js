53430112744128a45263b75962cc277fdf2284b4
Ira Karavaichyk <iragemini@gmail.com>
2020-09-29 10:06:34 +0300

    feat: Typical Arrays Problems task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    
+    if(Array.isArray(array) === false) return 0;
+    if(array.length === 0) return 0;
+    
+    array.sort((a, b) => b - a);
+    let positionMin = array.length - 1;
+    let minElem = array[positionMin];
+    return minElem;
 }
 
 exports.max = function max (array) {
-  return 0;
+    
+    if(Array.isArray(array) === false) return 0;
+    if(array.length === 0) return 0;
+
+    array.sort((a, b) => b - a);
+    let maxElem = array[0];
+    return maxElem;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    
+    if(Array.isArray(array) === false) return 0;
+    if(array.length === 0) return 0;
+
+    let sumElement = array.reduce((sum, current) => sum + current, 0);
+    let result = sumElement / array.length;
+    return result;
 }
